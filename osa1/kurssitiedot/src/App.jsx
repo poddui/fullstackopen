441dbab5666ee697f132f8b250ca2b9ba627c46a
poddui <jimi8888.podduikin@gmail.com>
2024-02-01 13:48:08 +0200

    osa1 teht 1-5

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = ({course}) => {
+  return(
+    <h1>{course.name}</h1>
+  )
+}
+
+const Part = ({part, exercises}) => {
+  return(
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Content = ({course}) => {
+  return(
+    <>
+      <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
+      <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = ({course}) => {
+  const totalExercises = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <>
+      <p>Number of exercises {totalExercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
+    </div>
+  )
+}
+
+export default App
